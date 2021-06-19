d716236b84b346bb3e518731fa3e6b28ba672f3a
Lê Minh Công <56527272+Cong-Le@users.noreply.github.com>
2021-06-19 13:22:34 +0700

    final commit

--- src/main/Config.java
+++ src/main/Config.java
@@ -15,40 +15,40 @@ public class Config {
     public static final int NorE_ID = 1;
     public static final int NorE_Health = 35;
     public static final int NorE_Reward = 5;
-    public static final double NorE_Speed = 2.2;
+    public static final int NorE_Speed = 2;
     //Tanker GameStage.Enemy
     public static final int TanE_ID = 2;
     public static final int TanE_Health = 50;
     public static final int TanE_Reward = 5;
-    public static final double TanE_Speed = 1.7;
+    public static final int TanE_Speed = 2;
     //Smaller GameStage.Enemy
     public static final int SmaE_ID = 3;
     public static final int SmaE_Health = 25;
     public static final int SmaE_Reward = 5;
-    public static final double SmaE_Speed = 3;
+    public static final int SmaE_Speed = 3;
     //Boss GameStage.Enemy
     public static final int BosE_ID = 4;
     public static final int BosE_Health = 150;
     public static final int BosE_Reward = 15;
-    public static final double BosE_Speed = 1.5;
+    public static final int BosE_Speed = 2;
 
     //Normal Tower
     public static final int NorT_ID = 1;
-    public static final int NorT_Damage = 4;
+    public static final int NorT_Damage = 5;
     public static final double NorT_Range = 180;
     public static final double NorT_Spaw = 25;   // Tốc độ sinh đạn, số càng lớn càng chậm
     public static final int NorT_Price = 40;
     //Sniper Tower
     public static final int SniT_ID = 2;
-    public static final int SniT_Damage = 6;
+    public static final int SniT_Damage = 7;
     public static final double SniT_Range = 250;
-    public static final double SniT_Spaw = 30;
+    public static final double SniT_Spaw = 35;
     public static final int SniT_Price = 50;
     //MachineGun Tower
     public static final int MacT_ID = 3;
-    public static final int MacT_Damage = 1;
+    public static final int MacT_Damage = 2;
     public static final double MacT_Range = 140;
-    public static final double MacT_Spaw = 12;
+    public static final double MacT_Spaw = 20;
     public static final int MacT_Price = 50;
 
     //Bullet
